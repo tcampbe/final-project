ba96ec701a10b39f64bc98b14111157e7878c8f5
tcampbe <tcampb@icloud.com>
2020-03-22 18:45:01 -0500

    cleaned up course controller - preparing to update db agian

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -16,15 +16,6 @@ namespace capstone.Controllers
 
     public class CourseController : ControllerBase
     {
-        //querying for catagories
-
-        //var blog = context.Blogs
-        //            .Where(b => b.Name == "ADO.NET Blog")
-        //            .FirstOrDefault();
-        //https://docs.microsoft.com/en-us/ef/core/querying/
-
-        //https://docs.microsoft.com/en-us/ef/ef6/querying/#finding-entities-using-a-query
-
         [HttpGet]
         public IEnumerable<Course> Get()
         {
@@ -36,35 +27,6 @@ namespace capstone.Controllers
             return courses;
 
         }
-
-        [HttpGet("category")]
-        public IEnumerable<Course> GetByCategory(string category)
-        {
-            Course[] categoryCourses = null;
-            using (var context = new ApplicationDbContext())
-            {
-                //var allCourses = context.Courses.Where(c => c.Category == "all");
-                //var englishCourses = context.Courses.Where(c => c.Category == "english");
-                //var mathCourses = context.Courses.Where(c => c.Category == "math");
-                //var scienceCourses = context.Courses.Where(c => c.Category == "science");
-                //var socialStudiesCourses = context.Courses.Where(c => c.Category == "socialStudies");
-                categoryCourses = context.Courses.Where(c => c.Category == category).ToArray();
-            }
-
-            return categoryCourses;
-
-        }
-
-        //[HttpGet("random")]
-        //public IActionResult GetRandomQuiz(int id)
-        //{
-        //    //ModelState.AddModelError("GetRandomQuiz", "Not Implemented!");
-        //    //return BadRequest(ModelState);
-        //    var quiz = _quizService.GetRandomQuiz();
-        //    return Ok(quiz.ToApiModel());
-
-        //    https://docs.microsoft.com/en-us/aspnet/core/web-api/action-return-types?view=aspnetcore-3.1
-        //}
 
         [HttpPost]
         public Course Post([FromBody]Course course)
